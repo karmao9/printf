fdf878a96d40fb880774c9c3cbbf8c6372d189ac
kamao9 <kamaliveo9@gmail.com>
2023-01-25 12:38:48 -0800

    printf.

--- _printf.c
+++ _printf.c
@@ -1,74 +1,68 @@
 #include "main.h"
+
+void print_buffer(char buffer[], int *buff_ind);
+
 /**
- * _printf - Printf function
- * @format: format type identifiers
- * Return: string with identifiers
- */
+* _printf - Printf function
+* @format: format.
+* Return: Printed chars.
+*/
+
 int _printf(const char *format, ...)
 {
+	int i, printed = 0, printed_chars = 0;
+	int flags, width, precision, size, buff_ind = 0;
 	va_list list;
-	unsigned int i = 0, length = 0, printed_chars = 0;
-	int (*func)(va_list, char *, unsigned int);
-	char *buffer;
+	char buffer[BUFF_SIZE];
+
+	if (format == NULL)
+		return (-1);
 
 	va_start(list, format);
-	buffer = malloc(sizeof(char) * 1024);
-	if (format == NULL || buffer == NULL ||
-	(format[i] == '%' && format[i + 1] == '\0'))
+
+	for (i = 0; format && format[i] != '\0'; i++)
 	{
-		return (-1);
-	}
-	if (format[i] == '\0')
-	{
-		return (0);
-	}
-	for (i = 0; format && format[i]; i++)
-	{
-		if (format[i] == '%')
+		if (format[i] != '%')
 		{
-			if (format[i + 1] == '%')
-			{
-				length = buffer_input(buffer, format[i], length);
-				i++;
-			}
-			if (format[i + 1] == '\0')
-			{
-				print_buffer(buffer, length);
-				free(buffer);
-				va_end(list);
-				return (-1);
-			}
-			else
-			{
-				func = get_func(format, i + 1);
-				if (func == NULL)
-				{
-					if (format[i + 1] == ' ' && !format[i + 2])
-					{
-						return (-1);
-					}
-					buffer_input(buffer, format[i], length);
-					printed_chars++;
-					i--;
-				}
-				else
-				{
-					printed_chars += func(list, buffer, length);
-					i += get_func1(format, i + 1);
-				}
-			}
-			i++;
+			buffer[buff_ind++] = format[i];
+			if (buff_ind == BUFF_SIZE)
+				print_buffer(buffer, &buff_ind);
+			/* write(1, &format[i], 1);*/
+			printed_chars++;
 		}
 		else
 		{
-			buffer_input(buffer, format[i], length);
-			printed_chars++;
+			print_buffer(buffer, &buff_ind);
+			flags = get_flags(format, &i);
+			width = get_width(format, &i, list);
+			precision = get_precision(format, &i, list);
+			size = get_size(format, &i);
+			++i;
+			printed = handle_print(format, &i, list, buffer,
+				flags, width, precision, size);
+			if (printed == -1)
+				return (-1);
+			printed_chars += printed;
 		}
-		for (length = printed_chars; length > 1024; length -= 1024)
-			;
-}
-	print_buffer(buffer, length);
-	free(buffer);
+	}
+
+	print_buffer(buffer, &buff_ind);
+
 	va_end(list);
+
 	return (printed_chars);
+}
+
+/**
+* print_buffer - Prints the contents of the buffer if it exist
+* @buffer: Array of chars
+* @buff_ind: Index at which to add next char, represents the length.
+*/
+
+void print_buffer(char buffer[], int *buff_ind)
+{
+	if (*buff_ind > 0)
+		write(1, &buffer[0], *buff_ind);
+
+	*buff_ind = 0;
 }
--- get_width.c
+++ get_width.c
@@ -1,4 +1,4 @@
-#include "2-main.h"
+#include "main.h"
 
 /**
  * get_width - Calculates the width for printing
@@ -10,27 +10,27 @@
  */
 int get_width(const char *format, int *i, va_list list)
 {
-int curr_i;
-int width = 0;
+	int curr_i;
+	int width = 0;
 
-for (curr_i = *i + 1; format[curr_i] != '\0'; curr_i++)
-{
-if (is_digit(format[curr_i]))
-{
-width *= 10;
-width += format[curr_i] - '0';
-}
-else if (format[curr_i] == '*')
-{
-curr_i++;
-width = va_arg(list, int);
-break;
-}
-else
-break;
-}
+	for (curr_i = *i + 1; format[curr_i] != '\0'; curr_i++)
+	{
+		if (is_digit(format[curr_i]))
+		{
+			width *= 10;
+			width += format[curr_i] - '0';
+		}
+		else if (format[curr_i] == '*')
+		{
+			curr_i++;
+			width = va_arg(list, int);
+			break;
+		}
+		else
+			break;
+	}
 
-*i = curr_i - 1;
+	*i = curr_i - 1;
 
-return (width);
+	return (width);
 }
